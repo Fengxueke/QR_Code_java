f8da30b964b83b07b371935f38b1e37bb583e748
lianweijie <lianweijie@fengxueke>
2016-06-20 19:59:47 +0800

    修改了部分注释

--- src/TD_model/TwoDimensionCode.java
+++ src/TD_model/TwoDimensionCode.java
@@ -124,6 +124,7 @@ public class TwoDimensionCode {
   public static void main (String[] args){
 	  String imgPath = "Michael_QRCode.png";
 	  String encoderContent = "Hello QRCode!!";
+	  //字符串不能过长-->此处还没去解决
 	  //11223344556677889911223344556677889911223344556677889
 	  TwoDimensionCode handler = new TwoDimensionCode();
 	  handler.encoderQRCode(encoderContent, imgPath,"png");
